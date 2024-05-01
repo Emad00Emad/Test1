a5642199594dfcd2fd9223ebd50cedfdb04ba1f7
ﻋُﹻﹻﻘﹻﹻﭑب ۦٰ۬ۦٰ۬ۦٰ۬ﮩﮩﮩ <163866570+Emad00Emad@users.noreply.github.com>
2024-05-01 18:30:09 +0300

    Update .tm.py

--- .tm.py
+++ .tm.py
@@ -1,10 +1,26 @@
-import os,sys
+import os,sys,requests,time
 
-os.system("clear")
-os.system("rm -rf Test1")
-os.system("clear")
-os.system("git clone https://github.com/Emad00Emad/Test1")
-os.system("clear")
-os.system("python .tm.py")
 
-print(" Hi bro ")
+
+def Emad():
+	if Emadman in requests.get("https://raw.githubusercontent.com/Emad00Emad/Test1/main/.m.txt").text:
+		os.system("python .tm.py")
+	else:
+		print(" Hello dear user, sorry, this code has been updated. Wait five minutes. The old code will be automatically deleted and the updated code will be installed from the programmer. [ Emad alkhashen ] Thank you.")
+		time.sleep(50)
+		os.system("clear")
+		os.system("rm -rf Test1")
+		os.system("clear")
+		os.system("git clone https://github.com/Emad00Emad/Test1")
+		os.system("clear")
+		os.system("python .tm.py")
+
+
+def Zz():
+	print(" Hi prooo ")
+
+
+if "__T__"==__T__:
+	try:Emadman = requests.get("https://raw.githubusercontent.com/Emad00Emad/Test1/main/.f.txt").text
+	except:pass
+	Emad()
