f0454fe22ee6a66780688de64fca00b9602eaa87
ﻋُﹻﹻﻘﹻﹻﭑب ۦٰ۬ۦٰ۬ۦٰ۬ﮩﮩﮩ <163866570+Emad00Emad@users.noreply.github.com>
2024-05-01 18:05:41 +0300

    Create .tm.py

---
+++ .tm.py
@@ -0,0 +1,10 @@
+import os,sys
+
+os.system("clear")
+os.system("rm -rf Test1")
+os.system("clear")
+os.system("git clone amamam")
+os.system("clear")
+os.system("python .tm.py")
+
+print(" Hi bro ")
