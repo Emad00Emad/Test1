514cfc9e2922fc465de882848c5ece63fe957fa3
ﻋُﹻﹻﻘﹻﹻﭑب ۦٰ۬ۦٰ۬ۦٰ۬ﮩﮩﮩ <163866570+Emad00Emad@users.noreply.github.com>
2024-05-01 18:06:05 +0300

    Update .tm.py

--- .tm.py
+++ .tm.py
@@ -3,7 +3,7 @@ import os,sys
 os.system("clear")
 os.system("rm -rf Test1")
 os.system("clear")
-os.system("git clone amamam")
+os.system("git clone https://github.com/Emad00Emad/Test1")
 os.system("clear")
 os.system("python .tm.py")
 
